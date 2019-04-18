dc5378ab013a77ae8b20d421446d833bdcb09bf3
zhangph1220 <zhangph1220@gmail.com>
2019-04-18 11:57:55 -0400

    add login & register

---
+++ react-front-end/src/components/auth/Register.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
+  }
+
+  render() {
+    // the same as 'const errors = this.state.errors'
+    const { errors } = this.state;
+
+    return (
+      <div className='register'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-8 m-auto'>
+              <h1 className='display-4 text-center'>Sign Up</h1>
+              <p className='lead text-center'>
+                Create your Columbia Forum account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
+                    placeholder='Name'
+                    name='name'
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                  {errors.name && (
+                    <div className='invalid-feedback'>{errors.name}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='email'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
+                    placeholder='Email Address'
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className='invalid-feedback'>{errors.email}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
+                    placeholder='Password'
+                    name='password'
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className='invalid-feedback'>{errors.password}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
+                    placeholder='Confirm Password'
+                    name='password2'
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                  />
+                  {errors.password2 && (
+                    <div className='invalid-feedback'>{errors.password2}</div>
+                  )}
+                </div>
+                <input type='submit' className='btn btn-info btn-block mt-4' />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
---
+++ react-front-end/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='bg-dark text-white mt-5 p-4 text-center'>
+      COMS 6998 - Cloud Computing Project - Columbia Forum
+    </footer>
+  );
+}
