357ec33759246b27346cf81bff8740f9772ed56b
Penghe <gaterw@hotmail.com>
2019-04-21 22:17:12 -0400

    add post

--- front-end-react/src/components/sections/Posts.js
+++ front-end-react/src/components/sections/Posts.js
@@ -1,43 +1,59 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
-const newsContent = [['test', 'content'], ['test', 'content']];
+import axios from 'axios';
 
 class Posts extends Component {
-  render() {
-    const newsContentElements = [];
-    for (let n of newsContent) {
-      newsContentElements.push(
-        <div className="card card-body mb-3">
-          <div className="row">
-            <div className="col-md-2">
-              <Link to="/home">
-                <img
-                  className="rounded-circle d-none d-md-block"
-                  src="https://www.gravatar.com/avatar/anything?s=200&d=mm"
-                />
-              </Link>
-              <br />
-              <p className="text-center">{n[0]}</p>
-            </div>
-            <div className="col-md-10">
-              <Link to="home">
-                <p className="lead">{n[1]}</p>
-                <p>Content</p>
-              </Link>
+  constructor() {
+    super();
+
+    this.state = {
+      newsContentElements: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/forum-post/all').then(res => {
+      console.log(res.data);
+      const newsContent = res.data;
+
+      const newsContentElements = [];
+      for (let n of newsContent) {
+        newsContentElements.push(
+          <div className="card card-body mb-3" key={n['PostID']}>
+            <div className="row">
+              <div className="col-md-2">
+                <Link to="/home">
+                  <img
+                    className="rounded-circle d-none d-md-block"
+                    alt=""
+                    src="https://www.gravatar.com/avatar/anything?s=200&d=mm"
+                  />
+                </Link>
+                <br />
+                <p className="text-center">{n['PostID']}</p>
+              </div>
+              <div className="col-md-10">
+                <Link to="/home">
+                  <p className="lead">{n['Title']}</p>
+                  <p>{n['Content']}</p>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      );
-    }
+        );
+      }
+      this.setState({ newsContentElements });
+    });
+  }
 
+  render() {
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="post-form mb-3">
             <div className="card card-info">
               <div className="card-header bg-info text-white">
-                Say Somthing...
+                Say Something...
               </div>
               <div className="card-body">
                 <form>
@@ -61,7 +77,7 @@ class Posts extends Component {
               </div>
             </div>
           </div>
-          <div className="posts">{newsContentElements}</div>
+          <div className="posts">{this.state.newsContentElements}</div>
         </div>
       </div>
     );
