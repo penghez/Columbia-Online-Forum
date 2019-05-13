b31b104473ffac8134b01d2e6558b8529bbf13b0
zhangph1220 <zhangph1220@gmail.com>
2019-05-12 20:21:21 -0400

    little modification on store js

--- front-end-react/src/store.js
+++ front-end-react/src/store.js
@@ -11,6 +11,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
+    // If you want to build in production, comment this line
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
